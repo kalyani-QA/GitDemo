99992a6651009008616ae0cb3a9bec902559e8eb
kalyani <ksamudrala02@gmail.com>
2020-07-16 12:14:16 -0500

    first commit

---
+++ src/gitStuff/Kalyani.java
@@ -0,0 +1,22 @@
+package gitStuff;
+
+import java.util.Scanner;
+
+public class Kalyani {
+	public static void main(String[] args) {
+		
+	      int count = 0;
+	      System.out.println("Enter a sentence :");
+	      Scanner sc = new Scanner(System.in);
+	      String s = sc.nextLine();
+
+	      for (int i=0 ; i<s.length(); i++){
+	         char ch = s.charAt(i);
+	         if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u'||ch == ' '){
+	            count ++;
+	         }
+	      }
+	      System.out.println("Number of vowels in the given sentence is "+count);
+	   }
+
+}
